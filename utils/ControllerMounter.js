cc6d6f550ce1cd82997034730e2172dac4ed4646
Vinorcola <vinorcola@gmail.com>
2017-09-26 22:36:53 +0200

    Enhance controller prefixing.

--- utils/ControllerMounter.js
+++ utils/ControllerMounter.js
@@ -31,58 +31,60 @@ const fetchControllers = (app, directoryPath, logger) => {
 }
 
 const mountController = (app, controller, logger) => {
-    let router = express.Router()
-
-    for (let action of controller.actions) {
+    controller.actions.map(action => {
+        let path = controller.prefix + action.path
         if (process.env.NODE_ENV === "development" && logger) {
-            let path = controller.prefix + action.path
             logger.info("Mount " + action.method.toUpperCase() + "\t" + (path === "" ? "/" : path))
         }
-        mountAction(router, action)
-    }
-    app.use(controller.prefix, router)
-}
 
-const mountAction = (router, action) => {
-    router[action.method.toLowerCase()](action.path, (request, response, next) => {
+        app[action.method.toLowerCase()](path, (request, response, next) => {
 
-        // Run authorizations.
-        if (action.authorization) {
-            if (!request.userAccount) {
-                next(new InvalidAuthenticationError("No authentication token provided."))
-                return
-            }
-            if (!request.userAccount.authorizations.includes(action.authorization)) {
-                next(new AccessDeniedError("Access denied to the resource.", action.authorization))
-                return
-            }
-        }
-
-        // Run validations.
-        let data = {}
-        let validations = []
-        if (action.validation) {
-            for (let attribute in action.validation) {
-                data[attribute] = request.params[attribute] || request.body[attribute]
-                for (let validation of action.validation[attribute]) {
-                    validations.push(validation(data[attribute]))
+            // Run authorizations.
+            if (action.authorization) {
+                if (!request.userAccount) {
+                    next(new InvalidAuthenticationError("No authentication token provided."))
+                    return
+                }
+                if (!request.userAccount.authorizations.includes(action.authorization)) {
+                    next(new AccessDeniedError("Access denied to the resource.", action.authorization))
+                    return
                 }
             }
-        }
-        Promise.all(validations)
 
-            // Call action handler with validated data, and authenticated user account.
-            .then(() => action.handler(data, request.userAccount, request.logger))
+            // Run validations.
+            let data = {}
+            let validations = []
+            if (controller.validation) {
+                for (let attribute in controller.validation) {
+                    data[attribute] = request.params[attribute] || request.body[attribute]
+                    for (let validation of controller.validation[attribute]) {
+                        validations.push(validation(data[attribute]))
+                    }
+                }
+            }
+            if (action.validation) {
+                for (let attribute in action.validation) {
+                    data[attribute] = request.params[attribute] || request.body[attribute]
+                    for (let validation of action.validation[attribute]) {
+                        validations.push(validation(data[attribute]))
+                    }
+                }
+            }
+            Promise.all(validations)
 
-            // Send result to response.
-            .then(result => {
-                response.json(Object.assign({
-                    ok: true,
-                }, result))
-            })
+                // Call action handler with validated data, and authenticated user account.
+                .then(() => action.handler(data, request.userAccount, request.logger))
 
-            // Catch any error.
-            .catch(next)
+                // Send result to response.
+                .then(result => {
+                    response.json(Object.assign({
+                        ok: true,
+                    }, result))
+                })
+
+                // Catch any error.
+                .catch(next)
+        })
     })
 }
 
